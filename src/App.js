3abc15eba8c167270613a30f51bfd1655bc84467
Pratik Salgiya <pratiksalgiya@gmail.com>
2019-05-23 17:30:41 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,34 @@
+import React from 'react';
+import ListComponent from './Components/ListComponent';
+import axios from 'axios';
+import './App.css';
 
+class App extends React.Component {
+  constructor(props){
+    super(props);
+        this.state={
+            apidata:[]
+        };
+    }
+    
+  componentWillMount(){
+    axios.get("https://jsonplaceholder.typicode.com/posts").then((response)=>{
+        console.log(response)
+            this.setState({
+                apidata:response.data
+                });
+                }
+            )
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <ListComponent apidata={this.state.apidata}></ListComponent>
+      </div>
+    );
+  }
+  
+}
+
+export default App;
